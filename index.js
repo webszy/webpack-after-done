cb143bb75d9471c6381c73543971e76914e651e1
webszy <sunzhenyu@live.com>
2020-10-22 20:21:28 +0800

    fix: source code lost

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
+
 module.exports = class done {
   constructor(arg) {
     //如果arg是数组的话
-    if(arg&&arg.length>0){
+    if (arg && arg.length > 0) {
       //TODO:将数组内元素promisify
-      this.funcList = arg.filter(e=>typeof e === 'function')
+      this.funcList = arg.filter(e => typeof e === 'function')
     } else {
-      this.funcList = []
-      // if(arg.zip){
-
-      // }
+      this.funcList = arg.funcList || []
     }
-
-
+    this.zipify = !!arg.zipify
+    this.zipFileName = arg.zipFileName
   }
   apply(compiler) {
-    console.log('compiler: ', compiler);
-    compiler.hooks.done.tap('done',() => {
+    // console.log('compiler: ', compiler.options.output.path);
+    compiler.hooks.done.tap('done', () => {
       console.log('compiler done')
-      if(this.funcList){
+      if (this.funcList) {
         //TODO: async await
-        for(let i =0;i<this.funcList.length;i++){
+        for (let i = 0; i < this.funcList.length; i++) {
           console.log(`function ${i} has running`)
           try {
             this.funcList[i]()
           } catch (error) {
             console.log('error: ', error);
           }
-          
+        }
+        if (this.zipify) {
+          const { makeZip } = require('./utils')
+          try {
+            makeZip(compiler.options.output.path, this.zipFileName)
+          } catch (error) {
+            console.log('error: ', error);
+          }
         }
       }
     })
