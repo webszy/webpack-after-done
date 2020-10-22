e54bbb4035fa7dc5393b430049f0c2106690765e
webszy <sunzhenyu@live.com>
2020-10-22 19:54:39 +0800

    update: readme and auto zip function

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# webpack_compiler_done
-my first webpack plugin:webpack_compiler_done,you will call functions when webpack compiler down
+# webpack-after-done
+my first webpack plugin:webpack-after-done,you will call functions when webpack compile done
+
+> basic use
+
+```javascript
+const AfterDone = require('webpack-after-done')
+const doneCallbacks = [function one(){},function two(){}]
+plugins.push(new AfterDone(doneCallbacks))
+```
+
+> optional use
+
+current support auto zip after you compile done,just send `zipify` and `zipFileName`
+
+```javascript
+const options = {
+  funcList:doneCallbacks,
+  zipify:true,
+  zipFileName:'dist.zip'
+}
+plugins.push(new AfterDone(options))
+```
--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 module.exports = class done {
-  constructor(funcList) {
-    if(funcList&&funcList.length>0){
-      this.funcList = funcList.filter(e=>typeof e === 'function')
+  constructor(arg) {
+    //如果arg是数组的话
+    if(arg&&arg.length>0){
+      //TODO:将数组内元素promisify
+      this.funcList = arg.filter(e=>typeof e === 'function')
     } else {
-      this.funcList = null
+      this.funcList = []
+      // if(arg.zip){
+
+      // }
     }
+
+
   }
   apply(compiler) {
+    console.log('compiler: ', compiler);
     compiler.hooks.done.tap('done',() => {
       console.log('compiler done')
       if(this.funcList){
+        //TODO: async await
         for(let i =0;i<this.funcList.length;i++){
           console.log(`function ${i} has running`)
           try {
             this.funcList[i]()
           } catch (error) {
             console.log('error: ', error);
-            
           }
           
         }
